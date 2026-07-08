ec669a4f339f046b75f3897e13cbf0d32a8b8064
MZH-MINT <zuhayr@mintsolutions.ae>
2026-07-08 11:37:36 +0530

    changes

--- app/public/wp-content/themes/mintsolutions-theme/pages/salesforce.php
+++ app/public/wp-content/themes/mintsolutions-theme/pages/salesforce.php
@@ -8,3 +8,233 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+$theme_uri = esc_url( get_template_directory_uri() );
+?>
+
+<section class="inner-banner-otr sales-f-banner">
+	<video autoplay muted loop playsinline>
+		<source src="<?php echo $theme_uri; ?>/assets/videos/sales-f-banner.mp4" type="video/mp4" />
+	</video>
+
+	<div class="container">
+		<div class="inner-banner-hero sales-f-hero">
+			<h2>Unlock the Full Potential of Salesforce</h2>
+			<h5>We offer a full range of Salesforce services designed to fitt all your requirements.</h5>
+
+			<div class="cards-container">
+				<section class="deck" id="deck">
+					<article class="panel active" style="background-image:url('<?php echo $theme_uri; ?>/assets/images/expand-img01.jpg')">
+						<div class="inner">
+							<h3>Platform <br /> Customization</h3>
+							<p class="desc">
+								Every Salesforce SaaS product is installed on Force.com platform and that allows users to build and
+								customize apps quickly using below ways
+							</p>
+						</div>
+					</article>
+
+					<article class="panel right" style="background-image:url('<?php echo $theme_uri; ?>/assets/images/expand-img02.jpg')">
+						<div class="inner">
+							<h3>Experience <br />Cloud</h3>
+							<p class="desc">
+								Salesforce Experience Cloud (formerly Community Cloud) is a digital experience platform that enables
+								organizations to create branded portals, websites, help centers, forums,
+							</p>
+						</div>
+					</article>
+
+					<article class="panel right" style="background-image:url('<?php echo $theme_uri; ?>/assets/images/expand-img04.jpg')">
+						<div class="inner">
+							<h3>Health <br />Cloud</h3>
+							<p class="desc">
+								Salesforce Experience Cloud (formerly Community Cloud) is a digital experience platform that enables
+								organizations to create branded portals, websites, help centers, forums,
+							</p>
+						</div>
+					</article>
+
+					<article class="panel right" style="background-image:url('<?php echo $theme_uri; ?>/assets/images/expand-img03.jpg')">
+						<div class="inner">
+							<h3>Salesforce<br />CPQ</h3>
+							<p class="desc">
+								Salesforce Experience Cloud (formerly Community Cloud) is a digital experience platform that enables
+								organizations to create branded portals, websites, help centers, forums,
+							</p>
+						</div>
+					</article>
+				</section>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="sales-f-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 mx-auto">
+				<div class="sales-f-container">
+					<div class="sf-head-section">
+						<h2>Drive Business <br /> Growth with <br /> Salesforce</h2>
+					</div>
+
+					<div class="sf-detailwrap">
+						<div class="sf-detailwrap-in">
+							<div class="breadcump"> /Inspire.Captivate. <span>Innovate/</span></div>
+							<p>
+								Accelerate Your Business Transformation with Mint solutions Salesforce Professional Services.
+								Enhance your customer experience and maximize ROI through our extensive Salesforce expertise
+							</p>
+							<ul>
+								<li>
+									<h3><span class="counter">30</span> <small>+</small></h3>
+									<h6>Projects</h6>
+								</li>
+								<li>
+									<h3><span class="counter">35</span> <small>+</small></h3>
+									<h6>Certifications</h6>
+								</li>
+								<li>
+									<h3><span class="counter">15</span> <small>+</small></h3>
+									<h6>Projects Delivered</h6>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="hover-gray-section" id="gh-num">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 mx-auto">
+						<div class="hover-detail-wrap">
+							<div class="hover-num">
+								<div class="gh-num-txt"></div>
+							</div>
+							<div class="hover-head">
+								<div class="hover-img"><img src="<?php echo $theme_uri; ?>/assets/images/icons/hand-icon.png" alt="Salesforce Partnership" /></div>
+								<div class="hover-dtl">
+									<div class="hover-title">Salesforce Partnership</div>
+									<div class="hover-par">Driving innovation and success for businesses worldwide</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hover-gray-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 mx-auto">
+						<div class="hover-detail-wrap">
+							<div class="hover-num">
+								<div class="gh-num-txt"></div>
+							</div>
+							<div class="hover-head">
+								<div class="hover-img"><img src="<?php echo $theme_uri; ?>/assets/images/icons/tr-angle.png" alt="On-Time Project Delivery" /></div>
+								<div class="hover-dtl">
+									<div class="hover-title">On-Time Project Delivery</div>
+									<div class="hover-par">Use AI-powered tools to ensure on-time project delivery</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hover-gray-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 mx-auto">
+						<div class="hover-detail-wrap">
+							<div class="hover-num">
+								<div class="gh-num-txt"></div>
+							</div>
+							<div class="hover-head">
+								<div class="hover-img"><img src="<?php echo $theme_uri; ?>/assets/images/icons/glob.png" alt="Global Salesforce Support" /></div>
+								<div class="hover-dtl">
+									<div class="hover-title">Global Salesforce Support</div>
+									<div class="hover-par">Cover all time zones to support and collaborate across geographies</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hover-gray-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 mx-auto">
+						<div class="hover-detail-wrap">
+							<div class="hover-num">
+								<div class="gh-num-txt"></div>
+							</div>
+							<div class="hover-head">
+								<div class="hover-img"><img src="<?php echo $theme_uri; ?>/assets/images/icons/tag.png" alt="In-house Code Development" /></div>
+								<div class="hover-dtl">
+									<div class="hover-title">In-house Code Development</div>
+									<div class="hover-par">Reduce the risks associated with third-party code</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hover-gray-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 mx-auto">
+						<div class="hover-detail-wrap">
+							<div class="hover-num">
+								<div class="gh-num-txt"></div>
+							</div>
+							<div class="hover-head">
+								<div class="hover-img"><img src="<?php echo $theme_uri; ?>/assets/images/icons/rbn.png" alt="Quality Assurance" /></div>
+								<div class="hover-dtl">
+									<div class="hover-title">Quality Assurance</div>
+									<div class="hover-par">Conduct in-house development and testing for reliable, efficient Salesforce Solutions</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="bottom-vdo-section">
+	<div class="container h-100">
+		<div class="vdo-content-wrap">
+			<h3>Discover how we can <br /> solve your business problems</h3>
+			<p>Schedule Your Free Salesforce Consultation Service Today!</p>
+			<a href="#enquirNow" class="rto-btn rto-wht-btn btn-x-bounce" data-bs-toggle="modal" data-bs-target="#enquirNow" data-modal-title="Talk to an Expert" data-no-body-padding="1">
+				<span>Talk to an Expert</span>
+			</a>
+		</div>
+	</div>
+
+	<video autoplay muted loop playsinline class="sales-vdo">
+		<source src="<?php echo $theme_uri; ?>/assets/videos/salesforce.mp4" type="video/mp4" />
+	</video>
+</div>
+
+<section>
+	<div class="btm-txt-scroll scrool-v" id="marquee">
+		<h2 class="text">High performing B2B Sales <span>Real Estate Agency CRM </span> Flight Booking Platform <span>High performing B2B Sales</span> A Connected Skier Experience <span>On-Demand Insurance</span> Real Estate Agency CRM <span>Flight Booking Platform</span> On-Demand Insurance <span>A Connected Skier Experience</span></h2>
+		<h2 class="text">High performing B2B Sales <span>Real Estate Agency CRM </span> Flight Booking Platform <span>High performing B2B Sales</span> A Connected Skier Experience <span>On-Demand Insurance</span> Real Estate Agency CRM <span>Flight Booking Platform</span> On-Demand Insurance <span>A Connected Skier Experience</span></h2>
+	</div>
+</section>
